a5ee0363aa7c52775f7a6f6514916c42d2395a12
Alexey <practicemakesperfect73@gmail.com>
2025-02-18 23:32:13 +0900

    Вывел кол-во активных пропусков

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
+
 if __name__ == '__main__':
-    # Программируем здесь
+    passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print('Активных пропусков:', len(active_passcards))
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
